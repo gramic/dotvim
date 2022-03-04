b0b573fd0b17f57ce425cf74ab3e00281f41e310
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2022-03-04 11:33:44 +0200

    Disable rainbow

--- lua/third-party-config/treesitter-setup.lua
+++ lua/third-party-config/treesitter-setup.lua
@@ -11,14 +11,14 @@ configs.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-  rainbow = {
-    enable = true,
-    -- disable = { "jsx", "cpp" }
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = 3000,
-    -- colors = {}, -- table of hex strings
-    -- termcolors = {} -- table of colour name strings
-  },
+  -- rainbow = {
+  --   enable = true,
+  --   -- disable = { "jsx", "cpp" }
+  --   extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+  --   max_file_lines = 3000,
+  --   -- colors = {}, -- table of hex strings
+  --   -- termcolors = {} -- table of colour name strings
+  -- },
   textobjects = {
     select = {
       enable = true,
