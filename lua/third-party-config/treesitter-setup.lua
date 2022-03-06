b7fee9b7e2774d93db78ea255514db4dcaed8522
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2022-03-06 21:29:11 +0200

    Add nvim-ts-autotag

--- lua/third-party-config/treesitter-setup.lua
+++ lua/third-party-config/treesitter-setup.lua
@@ -2,6 +2,9 @@ local configs = require'nvim-treesitter.configs'
 configs.setup {
   ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   -- ignore_install = { "javascript" }, -- List of parsers to ignore installing
+  autotag = {
+    enable = true,
+  },
   highlight = {
     enable = true,              -- false will disable the whole extension
     -- disable = { "c", "rust" },  -- list of language that will be disabled
