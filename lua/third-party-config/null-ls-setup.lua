088d2c0aa6532b2e80088cfc32291df899fd5d2d
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2022-03-30 22:03:11 +0300

    Install sqlfluff in null-ls-setup

--- lua/third-party-config/null-ls-setup.lua
+++ lua/third-party-config/null-ls-setup.lua
@@ -4,6 +4,9 @@ local null_ls = require("null-ls")
 local sources = {
   null_ls.builtins.diagnostics.buildifier,
   null_ls.builtins.formatting.buildifier,
+  null_ls.builtins.diagnostics.hadolint,
+  null_ls.builtins.diagnostics.protoc_gen_lint,
+  null_ls.builtins.diagnostics.sqlfluff
 }
 
 null_ls.setup({ sources = sources })
