df8704bf82d80c2baa228e5c3c8be99d852f6d5b
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2022-03-30 20:06:50 +0300

    Add formatting for buildifer in null-ls-setup

--- lua/third-party-config/null-ls-setup.lua
+++ lua/third-party-config/null-ls-setup.lua
@@ -3,6 +3,7 @@ local null_ls = require("null-ls")
 -- register any number of sources simultaneously
 local sources = {
   null_ls.builtins.diagnostics.buildifier,
+  null_ls.builtins.formatting.buildifier,
 }
 
 null_ls.setup({ sources = sources })
