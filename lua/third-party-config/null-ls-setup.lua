114e7518b09c3cd4997734576e61c3f47dae49af
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2022-04-02 15:27:06 +0300

    Add more null-ls extensions

--- lua/third-party-config/null-ls-setup.lua
+++ lua/third-party-config/null-ls-setup.lua
@@ -8,7 +8,12 @@ local sources = {
   null_ls.builtins.diagnostics.protoc_gen_lint,
   null_ls.builtins.diagnostics.sqlfluff,
   null_ls.builtins.diagnostics.tidy,
-  null_ls.builtins.diagnostics.yamllint
+  null_ls.builtins.diagnostics.yamllint,
+  null_ls.builtins.formatting.shfmt,
+  null_ls.builtins.formatting.shellharden,
+  null_ls.builtins.diagnostics.selene,
+  null_ls.builtins.formatting.stylua,
+  null_ls.builtins.diagnostics.codespell
 }
 
 null_ls.setup({ sources = sources })
