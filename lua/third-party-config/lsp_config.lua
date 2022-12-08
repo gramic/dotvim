f0bee657c3f821d55ee32c4e3649f46065ab60ea
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2022-12-08 11:20:09 +0200

    Configure telescope fzf native

--- lua/third-party-config/lsp_config.lua
+++ lua/third-party-config/lsp_config.lua
@@ -48,7 +48,10 @@ local servers = {
                     globals = { 'vim' },
                 },
                 workspace = {
-                    library = vim.api.nvim_get_runtime_file("", true),
+                  library = {
+                    [vim.fn.expand "$VIMRUNTIME/lua"] = true,
+                    [vim.fn.stdpath "config" .. "/lua"] = true,
+                  },
                 },
                 telemetry = {
                     enable = false,
