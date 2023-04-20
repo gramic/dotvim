056dffeed75717742438611b2cc979089e0c89ab
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2023-04-20 15:37:42 +0300

    Enable sadly noice plugin again

--- init.lua
+++ init.lua
@@ -31,14 +31,14 @@ require("lazy").setup({
   {
     "folke/noice.nvim",
     opts = {
-      enabled = false,
-      cmdline = {
-        enabled = false, -- enables the Noice cmdline UI
-      },
+      -- enabled = false,
+      -- cmdline = {
+      --   enabled = false, -- enables the Noice cmdline UI
+      -- },
       messages = {
         -- NOTE: If you enable messages, then the cmdline is enabled automatically.
         -- This is a current Neovim limitation.
-        enabled = false, -- enables the Noice messages UI
+        -- enabled = false, -- enables the Noice messages UI
         view = "notify", -- default view for messages
         view_error = "notify", -- view for errors
         view_warn = "notify", -- view for warnings
