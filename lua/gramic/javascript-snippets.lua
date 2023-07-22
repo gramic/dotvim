baf79e89d22ee16a19cfa5e7211ae12c445b467a
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2023-07-22 18:35:49 +0300

    Choice snippet works

--- lua/gramic/javascript-snippets.lua
+++ lua/gramic/javascript-snippets.lua
@@ -1,5 +1,6 @@
-local ts_query = require("vim.treesitter.query")
-local ts_languagetree = require("vim.treesitter.languagetree")
+-- local ts_query = require("vim.treesitter.query")
+-- local ts_languagetree = require("vim.treesitter.languagetree")
+-- local ts_utils = require("nvim-treesitter.ts_utils")
 local ls = require("luasnip")
 local s = ls.snippet
 local sn = ls.snippet_node
@@ -27,6 +28,40 @@ local fmt = require("luasnip.extras.fmt").fmt
 -- local parse = require("luasnip.util.parser").parse_snippet
 -- local ms = ls.multi_snippet
 
+---Returns index of the @param in the jsdoc comment
+---@param comment_node TSNode
+---@return integer
+local get_current_param_index_in_jsdoc = function(comment_node)
+  local query = vim.treesitter.query.parse(
+    "jsdoc",
+    [[
+((tag_name) @tag_name (#eq? @tag_name "@param"))
+]]
+  )
+  local comment_text = vim.treesitter.get_node_text(comment_node, 0, nil)
+  local jsdoc_parser =
+    vim.treesitter.get_string_parser(comment_text, "jsdoc", nil)
+  ---@type TSTree[]
+  local ts_tree = jsdoc_parser:parse()
+  local root_node = ts_tree[1]:root()
+  local start, _, stop, _ = vim.treesitter.get_node_range(root_node)
+  local bufnr = 0 -- current buffer
+  local param_captures = query:iter_captures(root_node, bufnr, start, stop)
+  local index = 0
+  for id, node, _ in param_captures do
+    local name = query.captures[id]
+    if name == "tag_name" then
+      local is_current_snippet = node:parent():child_count() < 2
+      if is_current_snippet then
+        return index + 1
+      else
+        index = index + 1
+      end
+    end
+  end
+  return 0
+end
+
 ---@param function_declaration_node TSNode
 ---@return string[]
 local get_function_declaration_parameter_names = function(
@@ -44,43 +79,44 @@ local get_function_declaration_parameter_names = function(
  )
    ]]
   )
-  for id, node, metadata in
-    query:iter_captures(function_declaration_node, 0, 0, 1000)
-  do
-    vim.print("node_type: ", node:type())
-    vim.print("metadata: ", metadata[id])
+  local start, _, stop, _ =
+    vim.treesitter.get_node_range(function_declaration_node)
+  local bufnr = 0 -- current buffer
+  local param_captures =
+    query:iter_captures(function_declaration_node, bufnr, start, stop)
+  for id, node, _ in param_captures do
     local name = query.captures[id]
     if name == "param" then
       local node_text = vim.treesitter.get_node_text(node, 0, nil)
-      vim.print("node text: ", node_text)
       table.insert(params, node_text)
     end
-    vim.print("param name: ", name)
   end
   return params
 end
 
+--- Return via tree-sitter what params the curent function has
+---@return table<string, string>
 local choice_existing_params = function(position)
   return d(position, function()
-    local parser = vim.treesitter.get_parser()
-    ---@type LanguageTree
-    local tree = parser:parse()
-    ---@type TSTree
-    local root = tree[1]:root()
+    ---@type TSNode[]
     local nodes = {}
     ---@type TSNode|nil
-    local current_node = vim.treesitter.get_node()
+    local current_node = vim.treesitter.get_node({ ignore_injection = false })
     if current_node == nil then
       table.insert(nodes, t("example"))
     elseif current_node:type() == "comment" then
+      local jsdoc_param_index = get_current_param_index_in_jsdoc(current_node)
       local function_declaration_node = current_node:next_named_sibling()
       if function_declaration_node:type() == "function_declaration" then
         local params =
           get_function_declaration_parameter_names(function_declaration_node)
-        for _, param_name in pairs(params) do
-          table.insert(nodes, t(param_name))
+        if jsdoc_param_index > 0 and #params >= jsdoc_param_index then
+          table.insert(nodes, t(params[jsdoc_param_index]))
+        else
+          for _, param_name in pairs(params) do
+            table.insert(nodes, t(param_name))
+          end
         end
-        -- table.insert(nodes, t("example3333"))
       end
     end
     return sn(nil, c(1, nodes))
@@ -117,7 +153,7 @@ ls.add_snippets("javascript", {
     i(1),
     t("} "),
     choice_existing_params(2),
-    t(" "),
+    t(""),
     i(0),
   }),
 })
