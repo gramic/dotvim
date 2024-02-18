e3454f57ebb0d55265ba25668fdaf212f557f404
Stanimir Mladenov <stanimir.mladenov@zoneprojects.com>
2024-02-18 11:58:29 +0200

    Make gx open with the open command.

--- init.lua
+++ init.lua
@@ -472,12 +472,6 @@ require("lazy").setup({
     },
   },
   {
-    "nvim-treesitter/playground",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
-  {
     "nvim-treesitter/nvim-treesitter",
     version = false, -- last release is way too old and doesn't work on Windows
     opts = function(_, opts)
@@ -634,15 +628,32 @@ require("lazy").setup({
   },
   {
     "chrishrb/gx.nvim",
-    event = { "BufEnter" },
-    opts = {
-      -- specify your browser app; default for linux "xdg-open"
-      open_browser_app = "open",
-      handlers = {
-        plugin = true, -- open plugin links in lua (e.g. packer, lazy, ..)
-        github = true, -- open github issues
-      },
-    },
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function()
+      vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = true, -- default settings
+    submodules = false, -- not needed, submodules are required only for tests
+    -- you can specify also another config if you want
+    config = function()
+      require("gx").setup({
+        open_browser_app = "open", -- specify your browser app; default for macOS is "open", Linux "xdg-open" and Windows "powershell.exe"
+        -- open_browser_args = { "--background" }, -- specify any arguments, such as --background for macOS' "open".
+        handlers = {
+          plugin = true, -- open plugin links in lua (e.g. packer, lazy, ..)
+          github = true, -- open github issues
+          brewfile = true, -- open Homebrew formulaes and casks
+          package_json = true, -- open dependencies from package.json
+          search = true, -- search the web/selection on the web if nothing else is found
+        },
+        handler_options = {
+          search_engine = "google", -- you can select between google, bing, duckduckgo, and ecosia
+          search_engine = "https://search.brave.com/search?q=", -- or you can pass in a custom search engine
+        },
+      })
+    end,
   },
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.editor.symbols-outline" },
